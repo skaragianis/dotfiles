f67d5c7f0c37108c5338e0055ea4f1774487b981
Steffan Karagianis <steffan.karagianis@gmail.com>
2026-07-13 14:47:36 +1000

    Added blink

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -196,6 +196,53 @@ require("lazy").setup({
       end,
     },
   },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "Kaiser-Yang/blink-cmp-dictionary",
+    },
+    opts = {
+      keymap = { preset = "super-tab" },
+      completion = {
+        ghost_text = { enabled = true },
+        list = { selection = { auto_insert = false } },
+        documentation = { auto_show = true, window = { border = "rounded" } },
+        menu = {
+          draw = {
+            padding = 0,
+            columns = { { "kind_icon", gap = 1 }, { gap = 1, "label" }, { "kind", gap = 2 } },
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  return " " .. ctx.kind_icon .. " "
+                end,
+                highlight = function(ctx)
+                  return "BlinkCmpKindIcon" .. ctx.kind
+                end,
+              },
+              kind = {
+                text = function(ctx)
+                  return " " .. ctx.kind .. " "
+                end,
+              },
+            },
+          },
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "dictionary" },
+        providers = {
+          dictionary = {
+            module = "blink-cmp-dictionary",
+            min_keyword_length = 3,
+          },
+        }
+      },
+      fuzzy = { implementation = "prefer_rust" },
+    },
+    opts_extend = { "sources.default" },
+  },
 })
 
 vim.cmd.colorscheme("catppuccin-mocha")
